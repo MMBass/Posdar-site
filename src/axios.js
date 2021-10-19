080234f323d76dcfddeb8eda3f866e7c87e915fb
unknown <mendibass@gmail.com>
2021-10-19 16:13:33 +0300

    axios url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const posdarUrlInstance = axios.create({
-    // baseURL: 'https://posdar.herokuapp.com',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://posdar.herokuapp.com',
+    // baseURL: 'http://localhost:5000',
     headers: {'X-Custom-Header': 'foobar'}
 });
